da9c5c00e14d0797681642e0a64e2ca1e9f55911
Bauffman <davy@bauffman.com>
2012-03-12 16:59:28 +0100

    Fix installer.

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-ln -s ~/dotfiles/bash/ ~/.bash
-ln -s ~/dotfiles/bash_profile ~/.bash_profile
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
-ln -s ~/dotfiles/inputrc ~/.inputrc
-ln -s ~/dotfiles/vim ~/.vim
-ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/.dotfiles/bash/ ~/.bash
+ln -s ~/.dotfiles/bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/gitignore_global ~/.gitignore_global
+ln -s ~/.dotfiles/inputrc ~/.inputrc
+ln -s ~/.dotfiles/vim ~/.vim
+ln -s ~/.dotfiles/vimrc ~/.vimrc
 echo "All files were installed"
